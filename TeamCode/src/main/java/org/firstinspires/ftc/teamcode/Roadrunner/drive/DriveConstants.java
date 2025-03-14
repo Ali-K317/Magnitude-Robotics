05e2a3bcc5304dcf48ec00de57d007ba51cfa7bc
Ali-K317 <akassmieh4233262@gmail.com>
2025-03-14 10:53:34 -0500

    Estimated the maximum velocity using the equation 0.8((Max RPM/60)*gear ratio*wheel radius*2PI)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/drive/DriveConstants.java
@@ -73,7 +73,7 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 30;
+    public static double MAX_VEL = 52;
     public static double MAX_ACCEL = 30;
     public static double MAX_ANG_VEL = Math.toRadians(60);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
