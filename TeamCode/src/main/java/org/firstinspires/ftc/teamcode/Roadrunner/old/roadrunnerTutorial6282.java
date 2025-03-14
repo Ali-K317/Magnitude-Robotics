efb8c3653c1cdd4530869d634af3c3f48c6c3fa4
Ali-K317 <akassmieh4233262@gmail.com>
2025-03-14 12:09:59 -0500

    Moved all old roadrunner stuff to another file to make room for updated roadrunner stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/old/roadrunnerTutorial6282.java
@@ -0,0 +1,18 @@
+//Credit for this tutorial goes to team 6282 simi vally robotics
+package org.firstinspires.ftc.teamcode.Roadrunner.old;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "First Roadrunner Tutorial", group = "Roadrunner")
+public class roadrunnerTutorial6282 extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+        while(opModeIsActive()){
+
+        }
+    }
+}
